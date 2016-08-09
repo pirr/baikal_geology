8296263a6d115edf74be31c36f8f6020a5eabb73
Aleksej Smaga <tattoo1mag@gmail.com>
2016-08-10 02:53:54 +0800

    algoritm is changed

--- anomaly_searcher.py
+++ anomaly_searcher.py
@@ -116,12 +116,13 @@ for f in uwb_logs_files:
 
 
 segments_dict = dict()
+jumps_dict = dict()
 limit = 100
 amplitude = 20
 for name, group in filegroups.items():
     sys.stdout.write(name + ':\n')
     segments_dict[name] = get_segments(limit, group[:3000], amplitude)
-    
+    segments_dict[name], jumps_dict[name] = merge_segments(segments_dict[name])
     sys.stdout.write("\033[K")
     sys.stdout.write('found {} segments in {} frames\n'.format(
         len(segments_dict[name]), len(group)))
