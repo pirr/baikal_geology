cc01b6413b6dbbbd63f14af394e591cc0ebcafe2
Aleksej Smaga <tattoo1mag@gmail.com>
2016-08-10 21:27:24 +0800

    not used

--- anomaly_searcher.py
+++ anomaly_searcher.py
@@ -2,36 +2,10 @@ import sys
 import os
 import numpy as np
 import pandas as pd
-import math
 import re
 
 
 def get_deduct(group, startframe, endframe):
-    rad = 6372795
-
-    lat1 = llat1 * math.pi / 180.
-    lat2 = llat2 * math.pi / 180.
-    long1 = llong1 * math.pi / 180.
-    long2 = llong2 * math.pi / 180.
-
-    cl1 = math.cos(lat1)
-    cl2 = math.cos(lat2)
-    sl1 = math.sin(lat1)
-    sl2 = math.sin(lat2)
-    delta = long2 - long1
-    cdelta = math.cos(delta)
-    sdelta = math.sin(delta)
-
-    y = math.sqrt(math.pow(cl2 * sdelta, 2) +
-                  math.pow(cl1 * sl2 - sl1 * cl2 * cdelta, 2))
-    x = sl1 * sl2 + cl1 * cl2 * cdelta
-    ad = math.atan2(y, x)
-    dist = ad * rad
-
-    return dist
-
-
-def get_deduct(group, si, count):
     deduct = 0
     for k in range(startframe, endframe):
         deduct += (group['thickness'].iloc[k] -
