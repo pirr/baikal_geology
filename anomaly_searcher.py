ada1a4838ea5b98d9a218ea261d574794881a451
aleksey <tattoo1mag@gmail.com>
2016-08-11 17:59:03 +0800

    performed only if the executable file

--- anomaly_searcher.py
+++ anomaly_searcher.py
@@ -75,70 +75,74 @@ def join_coords(del_df, gps_df):
     del_df = del_df.interpolate(method='linear', limit_direction='both')
     return del_df
 
-filegroups = dict()
-uwb_logs_folder = 'testdata'
-del_logs_files = (f for f in os.listdir(uwb_logs_folder) if f[-3:] == 'del')
-gps_logs_files = (f for f in os.listdir(uwb_logs_folder) if f[-3:] == 'gps')
 
-for f in del_logs_files:
-    del_log_df = pd.read_csv(os.path.join(uwb_logs_folder, f), sep=' ')
-    del_log_df = del_log_df.ix[:, 0:3]
-    del_log_df.columns = ['frame', 'min', 'max']
-    del_log_df.set_index(del_log_df['frame'], inplace=True)
-    f = f[:-3]
-    del_log_df['filename'] = f
-    gps_f = ''.join([f, 'gps'])
-    del_log_df['gpsfilename'] = gps_f
-    del_log_df['min'] = del_log_df['min'].str.replace(',', '.').astype(float)
-    del_log_df['max'] = del_log_df['max'].str.replace(',', '.').astype(float)
-    del_log_df['thickness'] = del_log_df.apply(
-        lambda row: (row['max'] - row['min']) * 8.93561103810775, axis=1)
-    gps_log_df = pd.read_csv(os.path.join(uwb_logs_folder, gps_f), sep=' ')
-    gps_log_df = gps_log_df.ix[:, 0:3]
-    gps_log_df.columns = ['frame', 'x', 'y']
-    gps_log_df['x'] = gps_log_df['x'].str.replace(',', '.').astype(float)
-    gps_log_df['y'] = gps_log_df['y'].str.replace(',', '.').astype(float)
-    del_log_df = join_coords(del_log_df, gps_log_df)
-    del_log_df = del_log_df.dropna()
+if name == '__main__':
+    filegroups = dict()
+    uwb_logs_folder = 'testdata'
+    del_logs_files = (f for f in os.listdir(uwb_logs_folder) if f[-3:] == 'del')
+    gps_logs_files = (f for f in os.listdir(uwb_logs_folder) if f[-3:] == 'gps')
 
-    filegroups[gps_f] = del_log_df
+    for f in del_logs_files:
+        del_log_df = pd.read_csv(os.path.join(uwb_logs_folder, f), sep=' ')
+        del_log_df = del_log_df.ix[:, 0:3]
+        del_log_df.columns = ['frame', 'min', 'max']
+        f = f[:-3]
+        del_log_df['filename'] = f
+        gps_f = ''.join([f, 'gps'])
+        del_log_df['min'] = del_log_df['min'].str.replace(',', '.').astype(float)
+        del_log_df['max'] = del_log_df['max'].str.replace(',', '.').astype(float)
+        del_log_df['thickness'] = del_log_df.apply(
+            lambda row: (row['max'] - row['min']) * 8.93561103810775, axis=1)
+        gps_log_df = pd.read_csv(os.path.join(uwb_logs_folder, gps_f), sep=' ')
+        gps_log_df = gps_log_df.ix[:, 0:3]
+        gps_log_df.columns = ['frame', 'x', 'y']
+        gps_log_df['x'] = gps_log_df['x'].str.replace(',', '.').astype(float)
+        gps_log_df['y'] = gps_log_df['y'].str.replace(',', '.').astype(float)
+        del_log_df = join_coords(del_log_df, gps_log_df)
+        del_log_df = del_log_df.dropna()
 
-
-segments_dict = dict()
-jumps_dict = dict()
-limit = 300
-amplitude = 20
-for name, group in filegroups.items():
-    sys.stdout.write(name + ':\n')
-    segments_dict[name] = get_segments(limit, amplitude, group)
-    segments_dict[name], jumps_dict[name] = merge_segments(segments_dict[name])
-    sys.stdout.write("\033[K")
-    sys.stdout.write(' ' * 10 + 'found {} segments in {} frames\n'.format(
-        len(segments_dict[name]), len(group)))
-
-draft_reestr = []
-for name, segments in segments_dict.items():
-    date = re.search(r'[0-9]{2}\.[0-9]{2}\.[0-9]{4}', name).group(0)
-    for k, segment in enumerate(segments):
-        min_val = np.fabs(segment['thickness'].min())
-        max_val = np.fabs(segment['thickness'].max())
-        draft_reestr.append([
-            segment['frame'].iloc[0],
-            segment['frame'].iloc[-1],
-            date, np.nan, 'прф',
-            np.nan, min_val, max_val,
-            max_val - min_val, np.median(segment['thickness']),
-            jumps_dict[name][k], np.nan,
-            name, 'gps, dat'
-        ])
+        filegroups[gps_f] = del_log_df
 
 
-df_protocol = pd.DataFrame(draft_reestr,
-                           columns=[
-                               'frame_st', 'frame_end',
-                               'date', 'time', 'type',
-                               'Z', 'tk_min', 'tk_max',
-                               'amplitude', 'middle',
-                               'jumps', 'other',
-                               'file_name', 'file_source'
-                           ])
+    segments_dict = dict()
+    jumps_dict = dict()
+    limit = 300
+    amplitude = 20
+    for name, group in filegroups.items():
+        sys.stdout.write(name + ':\n')
+        segments_dict[name] = get_segments(limit, amplitude, group[:3000])
+        segments_dict[name], jumps_dict[name] = merge_segments(segments_dict[name])
+        sys.stdout.write("\033[K")
+        sys.stdout.write(' ' * 10 + 'found {} segments in {} frames\n'.format(
+            len(segments_dict[name]), len(group)))
+
+    concat_segments = pd.concat([pd.concat(s) for s in segments_dict.values() if s],
+                                ignore_index=True)
+
+    concat_segments.to_csv('coords.csv', sep=';')
+
+    # draft_reestr = []
+    # for name, segments in segments_dict.items():
+    #     date = re.search(r'[0-9]{2}\.[0-9]{2}\.[0-9]{4}', name).group(0)
+    #     for k, segment in enumerate(segments):
+    #         min_val = np.fabs(segment['thickness'].min())
+    #         max_val = np.fabs(segment['thickness'].max())
+    #         draft_reestr.append([
+    #             segment['frame'].iloc[0],
+    #             segment['frame'].iloc[-1],
+    #             date, np.nan, 'прф',
+    #             np.nan, min_val, max_val,
+    #             max_val - min_val, np.median(segment['thickness']),
+    #             jumps_dict[name][k], np.nan,
+    #             name, 'gps, dat'
+    #         ])
+
+    # df_protocol = pd.DataFrame(draft_reestr,
+    #                            columns=[
+    #                                'frame_st', 'frame_end',
+    #                                'date', 'time', 'type',
+    #                                'Z', 'tk_min', 'tk_max',
+    #                                'amplitude', 'middle',
+    #                                'jumps', 'other',
+    #                                'file_name', 'file_source'
+    #                            ])
