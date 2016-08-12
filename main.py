57f4b17cfb8f0a059b71f954bce86e21c97dfe06
aleksey <tattoo1mag@gmail.com>
2016-08-12 16:20:57 +0800

    refactoring, fix issue
    fixed issue with type 'min', 'max' and coordinates

--- main.py
+++ main.py
@@ -2,32 +2,36 @@
 import os
 import sys
 import pandas as pd
+from functools import partial
 from multiprocessing import Pool
 
 from anomaly_searcher import get_segments, merge_segments, join_coords
 
 
-def multy_get_data(f, uwb_logs_folder='testdata'):
+def multy_get_data(uwb_logs_folder, f):
     del_log_df = pd.read_csv(os.path.join(uwb_logs_folder, f), sep=' ')
     del_log_df = del_log_df.ix[:, 0:3]
     del_log_df.columns = ['frame', 'min', 'max']
     f = f[:-3]
     del_log_df['filename'] = f
     gps_f = ''.join([f, 'gps'])
-    # del_log_df['gpsfilename'] = gps_f
-    del_log_df['min'] = del_log_df['min'].str.replace(',', '.').astype(float)
-    del_log_df['max'] = del_log_df['max'].str.replace(',', '.').astype(float)
+    del_log_df['min'] = del_log_df['min'].astype(
+        str).str.replace(',', '.').astype(float)
+    del_log_df['max'] = del_log_df['max'].astype(
+        str).str.replace(',', '.').astype(float)
     del_log_df['thickness'] = del_log_df.apply(
-        lambda row: (row['max'] - row['min']) * 8.93561103810775, axis=1)
+        lambda row: round((row['max'] - row['min']) * 8.93561103810775, 0), axis=1)
     gps_log_df = pd.read_csv(os.path.join(uwb_logs_folder, gps_f), sep=' ')
     gps_log_df = gps_log_df.ix[:, 0:3]
     gps_log_df.columns = ['frame', 'x', 'y']
-    gps_log_df['x'] = gps_log_df['x'].str.replace(',', '.').astype(float)
-    gps_log_df['y'] = gps_log_df['y'].str.replace(',', '.').astype(float)
+    gps_log_df['x'] = gps_log_df['x'].astype(
+        str).str.replace(',', '.').astype(float)
+    gps_log_df['y'] = gps_log_df['y'].astype(
+        str).str.replace(',', '.').astype(float)
     del_log_df = join_coords(del_log_df, gps_log_df)
     del_log_df = del_log_df.dropna()
 
-    return del_log_df
+    return del_log_df[['frame', 'filename', 'thickness', 'x', 'y']]
 
 
 def multy_segment_looker(filegroup, limit=300, amplitude=20, segments_dict=dict()):
