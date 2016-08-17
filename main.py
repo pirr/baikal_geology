46f345a5470da2f67cd13c61969fa217ebe0cb5b
aleksey <tattoo1mag@gmail.com>
2016-08-17 18:02:21 +0800

    df for protocol drafting

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import sys
 import pandas as pd
 from functools import partial
 from multiprocessing import Pool
+import numpy as np
+import re
 
 from anomaly_searcher import get_segments, merge_segments, join_coords
 
@@ -60,18 +62,44 @@ if __name__ == '__main__':
         anomaly_segments = [s for s in anomaly_segments if s is not None]
     sys.stdout.write('\nanomalys prepared')
 
-    jumps_dict = dict()
     if anomaly_segments:
-        anomalys_df = pd.concat(anomaly_segments[0][1][0], ignore_index=True)
-        jumps_dict[anomaly_segments[0][0]] = anomaly_segments[0][1][1]
+        anomalys_df = pd.concat(anomaly_segments[0]['anomaly_segments'][
+                                0], ignore_index=True)
     else:
         print('None anomalys')
         sys.exit()
 
-    for name, anomalys in anomaly_segments[1:]:
-        jumps_dict[name] = anomalys[1]
-        anomalys = pd.concat(anomalys[0], ignore_index=True)
+    for anomalys in anomaly_segments[1:]:
+        anomalys = pd.concat(anomalys['anomaly_segments'][0], ignore_index=True)
         anomalys_df = pd.concat([anomalys_df, anomalys], ignore_index=True)
 
-    anomalys_df.to_csv('ice_anomalies_120816_1335.csv', sep=';')
+    anomalys_df.to_csv('ice_anomalies_170816_1435.csv', sep=';')
+
+    draft_reestr = []
+    for anomalys in anomaly_segments:
+        date = re.search(r'[0-9]{2}\.[0-9]{2}\.[0-9]{4}',
+                         anomalys['name']).group(0)
+        for segment in anomalys['anomaly_segments']:
+            min_val = np.fabs(segment[0]['thickness'].min())
+            max_val = np.fabs(segment[0]['thickness'].max())
+            draft_reestr.append([
+                segment[0]['frame'].iloc[0],
+                segment[0]['frame'].iloc[-1],
+                date, np.nan, 'прф',
+                np.nan, min_val, max_val,
+                max_val - min_val, np.median(segment[0]['thickness']),
+                segment[1], np.nan,
+                anomalys['name'], 'gps, dat'
+            ])
+
+    df_protocol = pd.DataFrame(draft_reestr,
+                               columns=[
+                                   'frame_st', 'frame_end',
+                                   'date', 'time', 'type',
+                                   'Z', 'tk_min', 'tk_max',
+                                   'amplitude', 'middle',
+                                   'jumps', 'other',
+                                   'file_name', 'file_source'
+                               ])
+    df_protocol.to_csv('protocol_170816_1630.csv', sep=';')
     sys.stdout.write('\nDONE')
