4d74707e636ad0e55c9a638ec38581502d30a097
Aleksej Smaga <tattoo1mag@gmail.com>
2017-03-30 10:28:39 +0800

    reindex for objs_1

--- new_obj.py
+++ new_obj.py
@@ -42,15 +42,17 @@ def get_groups(dist_matrix, add_to_index_1=0, add_to_index_2=0):
 
     RX = np.where(R <= max_dis, R, np.NAN)
     true_indexes = np.argwhere(~np.isnan(RX))
-    true_indexes[:, 0] = true_indexes[:, 0] + add_to_index_1
-    true_indexes[:, 1] = true_indexes[:, 1] + add_to_index_2
 
-    nodes = np.union1d(true_indexes[:, 0], true_indexes[:, 1])
+    # _true_indexes = np.copy(true_indexes)
+    # _true_indexes[:, 0] = _true_indexes[:, 0] + add_to_index_1
+    # _true_indexes[:, 1] = _true_indexes[:, 1] + add_to_index_2
+
+    nodes = np.union1d(true_indexes[:, 0] + add_to_index_1, true_indexes[:, 1] + add_to_index_2)
 
     G = nx.Graph()
     G.add_nodes_from(nodes)
     for index in true_indexes:
-        G.add_edge(*index, weight=RX[index[0], index[1]])
+        G.add_edge(index[0] + add_to_index_1, index[1] + add_to_index_2, weight=RX[index[0], index[1]])
 
     min_weight_max_cliques = [cliq for cliq in get_min_weight_max_cliques(G) if len(cliq)>1]
 
@@ -64,7 +66,7 @@ if __name__ == '__main__':
 
     group_num = objs_1['group'].max(skipna=True)
     D = get_dist_between_objs(objs_1, objs_2)
-    min_weight_max_cliques_1 = get_groups(D, add_to_index_1=max(objs_2.index.values) + 1)
+    min_weight_max_cliques_1 = get_groups(D, add_to_index_1=max(objs_2.index.values)+1)
 
     d = get_dist_between_objs(objs_1)
     min_weight_max_cliques_2 = get_groups(d)
@@ -79,11 +81,12 @@ if __name__ == '__main__':
 
         objs_1.loc[objs_1['group'].isin(groups), 'group'] = gr_num
 
+    objs_1.index += max(objs_2.index.values) + 1
+    concat_objs = pd.concat([objs_2, objs_1])
+
     for gr in min_weight_max_cliques_1[0]:
-        groups_1 = objs_1.loc[gr, 'group'].dropna()
-        groups_2 = objs_2.loc[gr, 'group'].dropna()
-        conc_groups_val = list(groups_1.values) + list(groups_2.values)
-        conc_groups_val = [g for g in conc_groups_val if not pd.isnull(g)]
+        conc_groups = concat_objs.loc[gr, 'group'].dropna()
+        conc_groups_val = [g for g in conc_groups if not pd.isnull(g)]
         gr_num = group_num + 1
         for gr in conc_groups_val:
             if pd.isnull(gr):
@@ -91,8 +94,12 @@ if __name__ == '__main__':
             if gr_num > gr:
                 gr_num = gr
 
-        objs_1.loc[objs_1['group'].isin(groups_1), 'group'] = gr_num
-        objs_2.loc[objs_2['group'].isin(groups_2), 'group'] = gr_num
+        concat_objs.loc[concat_objs['group'].isin(conc_groups_val), 'group'] = gr_num
+
+    concat_objs.to_csv('vsegei_new_groups.csv', sep=';')
+
+
+
 
 
 
